4846123d2f999311e1f59e0d80cfeef93b9fe766
SandzoNight <flukemdc@gmail.com>
2017-03-16 16:47:52 +0700

    add view ,model package

--- src/view/ManageHor.java
+++ src/view/ManageHor.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package managehor;
+package view;
 
 import controller.DBConnector;
 import java.sql.Connection;
