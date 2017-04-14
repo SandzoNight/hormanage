22518520ed9c98c1306501daa98fafa50f226377
PJ <eye.patinthida@hotmail.com>
2017-04-14 22:00:08 +0700

    GUI ค่าใช้จ่ายห้องพัก

---
+++ src/view/dormitory/RoomCostController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.dormitory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.GridPane;
+
+public class RoomCostController implements Initializable {
+
+    @FXML
+    private Button paymentHistory;
+
+    @FXML
+    private GridPane roomCost;
+
+    @FXML
+    private Button invoice;
+
+    @FXML
+    private Hyperlink home;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+
+    }    
+    
+}
