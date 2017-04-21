537fbec89de0295d8e0371ea2e3d1c0d5087c725
PJ <eye.patinthida@hotmail.com>
2017-04-22 00:16:59 +0700

    สร้างใบแจ้งหนี้เสดแล้วน๊าอิอิ

--- src/controller/view/CreateInvoiceController.java
+++ src/controller/view/CreateInvoiceController.java
@@ -7,7 +7,12 @@ package controller.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,13 +20,48 @@ import javafx.fxml.Initializable;
  * @author CBC
  */
 public class CreateInvoiceController implements Initializable {
+    @FXML
+    private TextField electricityBill;
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField total;
+
+    @FXML
+    private TextField roomNumber;
+
+    @FXML
+    private TextField surname;
+
+    @FXML
+    private ComboBox<?> typeroom;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField roomRates;
+
+    @FXML
+    private Button reset;
+
+    @FXML
+    private TextField invoiceNo;
+
+    @FXML
+    private TextField waterBill;
+
+    @FXML
+    private Button comfirm;
+
+    @FXML
+    private Hyperlink home;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
 }
+
+
