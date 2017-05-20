647e8f33f2981db6878de9df7d24a156d00893e0
SandzoNight <flukemdc@gmail.com>
2017-05-20 19:33:13 +0700

    finish all features

--- src/controller/view/DormDashboardController.java
+++ src/controller/view/DormDashboardController.java
@@ -16,10 +16,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import model.DBConnector;
 import model.DataQuery;
 import view.dialog.DeleteBox;
@@ -65,6 +67,8 @@ public class DormDashboardController extends DormMainController implements Initi
     private Button editBtn;
     @FXML
     private Button toInvoiceListBtn;
+    @FXML
+    private Button printContractBtn;
     /**
      * Initializes the controller class.
      */
@@ -206,6 +210,22 @@ public class DormDashboardController extends DormMainController implements Initi
             //Change to new page
             System.out.println("[DormDashboardController]Changing scene..");
             window.setScene(scene);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void gotoViewContract(ActionEvent event) {
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            Parent root = loader.load(getClass().getResource("/view/dormitory/DormViewContract.fxml").openStream());
+            Stage newWindow = new Stage();
+            newWindow.setTitle("พิมพ์สัญญา | HOR Manager Application");
+            Scene scene = new Scene(root);
+            newWindow.setResizable(false);
+            newWindow.setScene(scene);
+            newWindow.show();
         }catch(IOException e){
             e.printStackTrace();
         }
--- src/controller/view/DormViewContractController.java
+++ src/controller/view/DormViewContractController.java
@@ -5,23 +5,103 @@
  */
 package controller.view;
 
+import controller.ContractManage;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.print.JobSettings;
+import javafx.print.PageLayout;
+import javafx.print.PageOrientation;
+import javafx.print.Paper;
+import javafx.print.Printer;
+import javafx.print.PrinterJob;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import controller.ContractManage;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import model.DataQuery;
 
 /**
  * FXML Controller class
  *
  * @author CBC
  */
-public class DormViewContractController implements Initializable {
+public class DormViewContractController extends DormDashboardController implements Initializable {
 
+    @FXML
+    private Label noContract;
+    @FXML
+    private Label startDate;
+    @FXML
+    private Label Date;
+    @FXML
+    private Label address;
+    @FXML
+    private Label address2;
+    @FXML
+    private Label address21;
+    @FXML
+    private Label address3;
+    @FXML
+    private ImageView print;
+    @FXML
+    private Button printBtn;
+    @FXML
+    private Pane printArea;
+    
+    private static long currentContractNo = 0;
+    private LocalDate currentTime;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Date date = new Date();
+        currentTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE dd MMMM yyyy");
+        Date.setText(sdf.format(date));
+        
+        ResultSet contractRes = DataQuery.queryContract(dormId);
+        try{
+            while(contractRes.next()){
+                currentContractNo = contractRes.getLong("contractNo")+1;
+            }
+            if(currentContractNo == 0){
+                currentContractNo = 1;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+        noContract.setText("CON-"+currentContractNo);
     }    
+
+    @FXML
+    private void print(ActionEvent event) {
+        PrinterJob job = PrinterJob.createPrinterJob();
+        JobSettings js = job.getJobSettings();
+        js.setJobName("Hor Manage Invoice");
+        PageLayout pl = js.getPageLayout();
+        Printer p = job.getPrinter();
+        pl = p.createPageLayout(Paper.A4, PageOrientation.PORTRAIT, 1,1,1.5,1.5);
+        js.setPageLayout(pl);
+        if(job != null && job.showPrintDialog(printArea.getScene().getWindow())){
+            boolean success = job.printPage(printArea);
+            if (success) {
+                System.out.println("End Print Job");
+                ContractManage.create(currentContractNo,currentTime,dormId);
+                job.endJob();
+            }
+        }
+    }
     
 }
